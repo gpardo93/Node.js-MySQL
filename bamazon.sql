807e55566e5df8b2a5a18b58e52e09c96e66ba82
Giovanni Pardo <giovanni_pardo@utexas.edu>
2018-01-31 16:18:18 -0600

    updated sql

--- bamazon.sql
+++ bamazon.sql
@@ -5,7 +5,7 @@ CREATE TABLE Products (ItemID int AUTO_INCREMENT, product_name varchar(50) NOT N
 
 use bamazon;
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Apple Watch", "Electronics & Computers", 329.00, 30);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Apple Watch", "Electronics & Computers", 2000.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Macbook Pro", "Electronics & Computers", 2000.00, 20);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Nikon D600", "Electronics & Computers", 2200.00, 10);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Apple Earpods", "Electronics & Computers", 179.00, 20);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Ibuprofen", "Pharmacy, Health & Beauty", 4.00, 25);
