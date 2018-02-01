c9ecdebb4fc3adf88966caee6266d22cf7d7e79d
Giovanni Pardo <giovanni_pardo@utexas.edu>
2018-02-01 16:28:20 -0600

    adding javascript

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,63 +14,34 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  runSearch();
-});
+  if(err){
+      console.log('Not connecting, try again');
+      return;
+  }
+      console.log('Congrats, you connected');
 
-function runSearch() {
-  inquirer
-    .prompt({
-      name: "action",
-      type: "rawlist",
-      message: "What would you like to do?",
-      choices: [
-        "Find songs by artist",
-        "Find all artists who appear more than once",
-        "Find data within a specific range",
-        "Search for a specific song",
-        "Find artists with a top song and top album in the same year"
-      ]
-    })
-    .then(function(answer) {
-      switch (answer.action) {
-        case "Find songs by artist":
-          artistSearch();
-          break;
-
-        case "Find all artists who appear more than once":
-          multiSearch();
-          break;
-
-        case "Find data within a specific range":
-          rangeSearch();
-          break;
-
-        case "Search for a specific song":
-          songSearch();
-          break;
-
-        case "Find artists with a top song and top album in the same year":
-          songAndAlbumSearch();
-          break;
-      }
-    });
-}
-
-function artistSearch() {
-  inquirer
-    .prompt({
-      name: "artist",
-      type: "input",
-      message: "What artist would you like to search for?"
-    })
-    .then(function(answer) {
-      var query = "SELECT position, song, year FROM top5000 WHERE ?";
-      connection.query(query, { artist: answer.artist }, function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+var schema = {
+    properties: {
+        ID: {
+        message: "Enter the ID of the product you would like to buy.",
+        pattern: /^[0-9][0-9]$|^[0-9]$/,
+        required: true
+        },
+        howMany: {
+        message: "Enter how many items you would like to buy.",
+        pattern: /^[0-9][0-9]$|^[0-9][0-9][0-9]$/,
+        required: true
         }
-        runSearch();
-      });
-    });
+    }
+};
+
+var schema2 = {
+    properties: {
+        AnotherPurchase: {
+        message: "Would you like to buy another item?.",
+        pattern: /(no|n|yes|y)/,
+        required: true
+        },
+    }
 }
+});
